14352c8066f13d0ea87cf1fa45683d326008c56e
minnieming <eunmilee9109.1@gmail.com>
2024-05-22 23:54:57 +0900

    X exception 로그에만 찍힘 -> 추후에 다시 도전

---
+++ src/main/java/com/sparta/devcampauth/signup/exception/ClientException.java
@@ -0,0 +1,17 @@
+//package com.sparta.devcampauth.signup.exception;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import org.springframework.http.HttpStatus;
+//
+//@Getter
+//public class ClientException extends RuntimeException {
+//
+//  private ErrorCode errorCode;
+//
+//  public ClientException (ErrorCode errorCode) {
+//      super(errorCode.getMessage());
+//      this.errorCode = errorCode;
+//  }
+//
+//}
---
+++ src/main/java/com/sparta/devcampauth/signup/exception/ErrorCode.java
@@ -0,0 +1,16 @@
+//package com.sparta.devcampauth.signup.exception;
+//
+//import com.sparta.devcampauth.signup.dto.SignupRequest;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import org.springframework.http.HttpStatus;
+//
+//@Getter
+//@AllArgsConstructor
+//public enum ErrorCode {
+//
+//    DUPLICATE_EMAIL (HttpStatus.CONFLICT, "이미 존재하는 이메일입니다.");
+//
+//    private final HttpStatus httpStatus;
+//    private final String message;
+//    }
