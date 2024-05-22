9ff42d7073af98b0c1617c249007064095a5fcb9
minnieming <eunmilee9109.1@gmail.com>
2024-05-22 20:51:59 +0900

    first commit

---
+++ src/main/java/com/sparta/devcampauth/DevCampAuthApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.devcampauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevCampAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevCampAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/devcampauth/DevCampAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.devcampauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevCampAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
